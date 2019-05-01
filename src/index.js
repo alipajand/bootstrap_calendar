30ec5183303fca181ec7306d78a2d586c94240cf
alipajand <ali.pajand@gmail.com>
2019-05-01 09:42:52 +0430

    :ambulance: Fix a bug adding option to get prototype of primary color

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import CalendarComponent from './components/calendar';
 
-// This exports the plugin object.
 export default {
-    // The install method will be called with the Vue constructor as
-    // the first argument, along with possible options
     install(Vue, options) {
         Vue.component('CalendarComponent', CalendarComponent);
-
-        const { primaryColor } = options;
-        Vue.prototype.$primaryColor = primaryColor;
     }
 };
